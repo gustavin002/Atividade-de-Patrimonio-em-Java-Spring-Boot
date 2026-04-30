5d9fe7d14858bc37dd30e4111e06e7229af604ee
joaovitorcosta29 <joaovitorcostalima81@gmail.com>
2026-04-30 15:31:32 -0300

    equipamentoBean

--- src/main/java/com/projeto/patrimonio/model/EquipamentoBean.java
+++ src/main/java/com/projeto/patrimonio/model/EquipamentoBean.java
@@ -9,5 +9,51 @@ package com.projeto.patrimonio.model;
  * @author Aluno
  */
 public class EquipamentoBean {
-    
+    private int idEquipamento;
+    private String codigoPatrimonio;
+    private String tipo;
+    private String marca;
+
+    public EquipamentoBean(int idEquipamento, String codigoPatrimonio, String tipo, String marca) {
+        this.idEquipamento = idEquipamento;
+        this.codigoPatrimonio = codigoPatrimonio;
+        this.tipo = tipo;
+        this.marca = marca;
+    }
+
+    public EquipamentoBean() {
+    }
+
+    public int getIdEquipamento() {
+        return idEquipamento;
+    }
+
+    public void setIdEquipamento(int idEquipamento) {
+        this.idEquipamento = idEquipamento;
+    }
+
+    public String getCodigoPatrimonio() {
+        return codigoPatrimonio;
+    }
+
+    public void setCodigoPatrimonio(String codigoPatrimonio) {
+        this.codigoPatrimonio = codigoPatrimonio;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
 }
