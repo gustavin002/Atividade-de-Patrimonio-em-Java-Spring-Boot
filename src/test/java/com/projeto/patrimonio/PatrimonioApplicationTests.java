b6fe0e8cd42ff261163c5af46aec5cb86bcdced5
Luiz Otávio <luiz.fontes0@outlook.com>
2026-04-30 14:49:40 -0300

    iniciando o projeto

---
+++ src/test/java/com/projeto/patrimonio/PatrimonioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.patrimonio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatrimonioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
